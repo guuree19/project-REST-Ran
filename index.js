702d71bbaf23daba1384f99dc45d35c9cf606da7
Abdulaziz Ali <guuree1982@gmail.com>
2022-12-30 21:00:44 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const express = require('express')
 
 // Initialize the app object.
 const app = express()
+
+// defines the view engine (JSX in this case) 
+app.set('view engine' , 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 // Create a homepage route.
 app.get('/',  (req, res) =>{
     // This gets sent to the client 
     // (your web browser most likely!)
-    res.send('Hello world')
+    res.render('home')
 })
 
 app.get('*',  (req, res) =>{
