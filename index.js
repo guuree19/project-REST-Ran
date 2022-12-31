91190c5678f8a4cf4004cd62a1efb57d70fe5808
Abdulaziz Ali <guuree1982@gmail.com>
2022-12-30 21:10:11 -0500

    Adding 404 page view

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express')
 // Initialize the app object.
 const app = express()
 
-// defines the view engine (JSX in this case) 
+// defined the view engine (JSX in this case) 
 app.set('view engine' , 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
@@ -14,14 +14,12 @@ app.use('/places', require('./controllers/places'))
 // Create a homepage route.
 app.get('/',  (req, res) =>{
     // This gets sent to the client 
-    // (your web browser most likely!)
     res.render('home')
 })
 
 app.get('*',  (req, res) =>{
     // This gets sent to the client 
-    // (your web browser most likely!)
-    res.status(404).send('<h1> 404 page</h1>')
+    res.render('error404')
 })
 
 
