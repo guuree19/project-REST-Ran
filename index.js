d2ffabaa931fe6edfb9add51662e87336bca3399
Abdulaziz Ali <guuree1982@gmail.com>
2023-01-07 11:44:12 -0500

    Form data is getting through!

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require('dotenv').config()
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 // Create a homepage route.
