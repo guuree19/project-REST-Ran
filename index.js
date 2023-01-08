01b2c1efd40a148f8574c937ba6bb01058db01f9
Abdulaziz Ali <guuree1982@gmail.com>
2023-01-08 11:49:20 -0500

    edit and delete buttons added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express')
 // Initialize the app object.
 const app = express()
 require('dotenv').config() 
+const methodOverride = require("method-override");
 const bodyParser = require('body-parser');
 
 
@@ -13,6 +14,7 @@ app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"));
 
 app.use('/places', require('./controllers/places'))
 // Create a homepage route.
